09fbf836347b061921cd320a0471cdbb0b1dcbd2
Paolo Rechia <paolorechia@gmail.com>
2023-08-20 22:29:20 +0200

    Command buffer

--- src/components/block.rs
+++ src/components/block.rs
@@ -8,21 +8,34 @@ pub fn Block() -> Html {
     let command_buffer: UseStateHandle<String> = use_state(|| String::from(""));
     let is_typing_command: UseStateHandle<bool> = use_state(|| false);
 
-    let onchange_handler = {
+    let onkeydown_handler = {
+        let new_is_typing_command = is_typing_command.clone();
+        let new_command_buffer = command_buffer.clone();
+        Callback::from(move | e:KeyboardEvent| {
+            if e.code() == "Slash" {
+                info!("Detected slash, should check for command");
+                new_is_typing_command.set(true);
+                e.prevent_default();
+            }
+
+            if *new_is_typing_command == true {
+                e.prevent_default();
+                if e.code() == "Escape" {
+                    info!("Detected escape, should cancel command");
+                    new_is_typing_command.set(false);
+                    new_command_buffer.set(String::from(""));
+                };
+            }
+        })
+    };
+
+    let onkeyup_handler = {
         let new_state_string = state_string.clone();
         let new_command_buffer = command_buffer.clone();
         let new_is_typing_command = is_typing_command.clone();
 
         Callback::from(move | e:KeyboardEvent| {
-            if e.code() == "Slash" {
-                info!("Detected slash, should check for command");
-                is_typing_command.set(true);
-            }
-            if e.code() == "Escape" {
-                info!("Detected escpae, should cancel command");
-                is_typing_command.set(false);
-            }
-            if *is_typing_command == true {
+            if *new_is_typing_command == true {
                 // Digits or lower case letters
                 if (e.which() >= 48 && e.which() <= 57) || (e.which() >= 65 && e.which() <= 90) {
                     info!("Detected letter/digit");
@@ -35,17 +48,20 @@ pub fn Block() -> Html {
                 let val = input.value();
                 new_state_string.set(val);
                 info!("{:?}", *new_command_buffer);
-                info!("{:?}", *is_typing_command);
+                info!("{:?}", *new_is_typing_command);
             }
         })
     };
     let string_copy = (*state_string).clone();
     let command_buffer_copy = (*command_buffer).clone();
+    let is_typing_command_copy = (*is_typing_command).clone();
     html! {
         <main>
-            <textarea onkeyup={onchange_handler.clone()}></textarea>
+            <textarea onkeydown={onkeydown_handler.clone()} onkeyup={onkeyup_handler.clone()}></textarea>
             <div> {"You've typed: "}{string_copy} </div>
-            <div> {"Your command buffer: "}{command_buffer_copy} </div>
+            if is_typing_command_copy {
+                <div> {"Your command buffer: "} {command_buffer_copy} </div>
+            }
         </main>
     }
 }
